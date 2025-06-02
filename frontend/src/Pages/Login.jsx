60c0084c1fc8cc9c6919c07ec35242fc1b6fff81
Sarthak Yash Srivastava <sarthak.nov21@gmail.com>
2025-06-03 00:55:05 +0530

    Some final Changes in local Enviornment

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -53,7 +53,7 @@ function Login() {
     <div>
       <Navbar/>
       <div className='w-full h-full flex justify-center items-center'>
-        <div className='md:w-[60vh] md:h-[45vh] w-[30vh] h-[40vh] shadow-2xl rounded-2xl md:mt-[30vh] mt-[60%] glow bg-slate-50'>
+        <div className='md:w-[60vh] md:h-[45vh] w-[30vh] h-[36vh] shadow-2xl rounded-2xl md:mt-[30vh] mt-[60%] glow bg-slate-50'>
           <p className='text-center text-2xl p-3 font-serif mt-5'>Login to Continue</p>
           <form className='flex-col p-4'>
             <p className='text-xl p-2'>User-ID</p>
